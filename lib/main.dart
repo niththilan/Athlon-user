6aed8fe92ee7897bd287371f58f10903c8fa8384
Nishva <knishvaraj@gmail.com>
2025-08-09 01:58:16 +0530

    app name and app icon added

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Athlon User',
+      title: 'Athlon',
       navigatorKey: NavigationService.navigatorKey,
       theme: ThemeData(
         useMaterial3: true,
