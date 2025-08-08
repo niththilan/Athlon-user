4bfb655eb292c12a4d645c2b9f4e78e3d5b68c63
Nishva <knishvaraj@gmail.com>
2025-08-08 19:14:18 +0530

    main is only runnable now

--- lib/customers/home.dart
+++ lib/customers/home.dart
@@ -49,38 +49,6 @@ class NoAnimationRoute<T> extends PageRoute<T> {
   bool get barrierDismissible => false;
 }
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Remove the _isLoading check from here
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1B2C4F),
-          brightness: Brightness.light,
-          background: const Color.fromARGB(255, 34, 51, 117),
-        ),
-        scaffoldBackgroundColor: const Color(0xFFF5F6FA),
-        cardTheme: CardThemeData(
-          elevation: 4,
-          shadowColor: Colors.black26,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-        ),
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
